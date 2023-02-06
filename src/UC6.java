e379ad7d5ae9f505bc415e7ba21ceb9301f06b38
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-06 16:13:13 +0530

    Shows Luckiest & Un-Luckiest Day

--- src/UC6.java
+++ src/UC6.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class UC5{
+public class UC6 {
     public static void main(String[] args) {
         int stake = 100;
         int betValue = 1;
@@ -12,6 +12,8 @@ public class UC5{
         int totalLoose = 0;
         int winDays = 0;
         int looseDays = 0;
+        int[] winBets = new int[21];
+        int[] looseBets = new int[21];
 
         while (totalDays <= 20) {
             win = 0;
@@ -37,13 +39,13 @@ public class UC5{
                 System.out.println("Gambler won " +(win - loose) +"$ on day " + totalDays);
                 winDays++;
                 totalWin += (win - loose);
-
+                winBets[totalDays] = win;
             }
             else{
                 System.out.println("Gambler looses " +(loose - win) + "$ on day " + totalDays );
                 looseDays++;
                 totalLoose += (loose - win);
-
+                looseBets[totalDays] = loose;
             }
             totalDays++;
 
@@ -53,6 +55,26 @@ public class UC5{
         System.out.println("Total Amount won by Gambler in 20 days : " + totalWin + "$");
         System.out.println("Total days loosed By gambler : " +looseDays);
         System.out.println("Total Amount loosed by Gambler in 20 days : " + totalLoose + "$");
+        System.out.println("\nwon bets : " + Arrays.toString(winBets));
+        System.out.println("Loose bets : " + Arrays.toString(looseBets));
+        int maxWin = winBets[0];
+        int maxLoose = looseBets[0];
+        int maxWinDay = 0;
+        int maxlooseDay = 0;
+
+        for(int i = 0; i < winBets.length; i++){
+            if(winBets[i] > maxWin){
+                maxWin = winBets[i];
+                maxWinDay = i;
+            }
+            if (looseBets[i]> maxLoose){
+                maxLoose = looseBets[i];
+                maxlooseDay = i;
+
+            }
+        }
+        System.out.println( "\nluckiest Day : Day " +maxWinDay + " gambler won maximum bets which is " +maxWin);
+        System.out.println( "Unluckiest Day : Day " +maxlooseDay + " gambler looses maximum bets which is " +maxLoose);
 
 
     }
